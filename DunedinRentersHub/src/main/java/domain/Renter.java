c8a207d7ca543a9118038a3ec5fecfa6bf722a12
zotge698 <zotge698@student.otago.ac.nz>
2021-04-01 00:09:05 +1300

    Renter domain class added.
    #2

---
+++ DunedinRentersHub/src/main/java/domain/Renter.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author zotta
+ */
+public class Renter {
+    
+    private String username;
+    private String email;
+    private String phone;
+
+    public Renter() {
+    }
+
+    public Renter(String username, String email, String phone) {
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+    
+    
+}
