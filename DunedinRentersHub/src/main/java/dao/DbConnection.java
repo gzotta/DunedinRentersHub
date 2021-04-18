6c2c1c64c10381d8a0ce1f4f94ff03811ec92b6a
Max Roberts <robma520@student.otago.ac.nz>
2021-04-19 11:37:18 +1200

    added db connection class
    references #3

---
+++ DunedinRentersHub/src/main/java/dao/DbConnection.java
@@ -0,0 +1,38 @@
+
+import dao.DAOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.h2.jdbcx.JdbcConnectionPool;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class DbConnection {
+
+   private static final String USERNAME = "sa";
+   private static final String PASSWORD = "sa";
+
+   private static final Integer DB_PORT = 9092;
+   private static final String DEFAULT_URI = "jdbc:h2:tcp://localhost:" + DB_PORT + "/project";
+
+   private static JdbcConnectionPool pool;
+
+   public static Connection getConnection(String uri) {
+
+      if (pool == null) {
+         pool = JdbcConnectionPool.create(uri, USERNAME, PASSWORD);
+      }
+
+      try {
+         return pool.getConnection();
+      } catch (SQLException ex) {
+         throw new DAOException("error connecting to database",ex);
+      }
+   }
+
+   public static String getDefaultConnectionUri() {
+      return DEFAULT_URI;
+   }
+}
