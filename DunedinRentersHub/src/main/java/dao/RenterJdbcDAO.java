a37200cdfef1ac1d3a208664a82a6f3ebadc1620
Max Roberts <robma520@student.otago.ac.nz>
2021-04-24 15:46:12 +1200

    added getRenterWishlist method
    references #4

--- DunedinRentersHub/src/main/java/dao/RenterJdbcDAO.java
+++ DunedinRentersHub/src/main/java/dao/RenterJdbcDAO.java
@@ -1,11 +1,16 @@
 package dao;
 
+import domain.Landlord;
+import domain.Property;
 import domain.Renter;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
  
 /**
  *
@@ -74,7 +79,6 @@ public class RenterJdbcDAO {
                     String renterPhone = rs.getString("renterPhone");
                     String renterEmail = rs.getString("renterEmail");
                     String references = rs.getString("references");
-                  //  ArrayList<Property> wishList = rs.getString("wishList");    // implement wishlist later
 
                     // use the data to create a renter object
                     Renter r = new Renter(renterId1, renterPassword, userName, dateOfBirth, renterPhone, renterEmail, references);
@@ -88,6 +92,57 @@ public class RenterJdbcDAO {
                 throw new DAOException(ex.getMessage(), ex);
             }
     }
+    
+    //method to return properties filtered by number of bedrooms
+    public Collection<Property> getRenterWishlist(Renter r) {
+
+        String sql =  "SELECT Poperty.propertyId, Poperty.landlordId, Poperty.bedrooms, Poperty.address, Property.status FROM PROPERTY left outer join WISHLIST ON Property.propertyId = Wishlist.propertyId where Wishlist.RenterId = ?";
+
+        try (
+                // get a connection to the database
+                Connection dbCon = DbConnection.getConnection(databaseURI);
+                // create the statement
+                PreparedStatement stmt = dbCon.prepareStatement(sql);) {
+            // copy the data from the product domain object into the SQL parameters
+            stmt.setInt(1, r.getRenterId());
+            // execute the query
+            ResultSet rs = stmt.executeQuery();
+
+            // Using a List to preserve the order in which the data was returned from the query.
+            List<Property> properties = new ArrayList<>();
+
+            // iterate through the query results
+            while (rs.next()) {
+
+                // get the data out of the query
+                Integer propertyId = rs.getInt("propertyId");
+                Integer landlordId = rs.getInt("landlordId");
+                Integer propertyBedrooms = rs.getInt("bedrooms");
+                String address = rs.getString("address");
+                String status = rs.getString("status");
+
+                // use the data to create a property object
+                Property p = new Property();
+                p.setPropertyId(propertyId);
+                p.setLandlordId(landlordId);
+                p.setBedrooms(propertyBedrooms);
+                p.setAddress(address);
+                p.setStatus(status);
+
+                // and put it in the collection
+                properties.add(p);
+            }
+
+            //return collection of properties that the renter has on their wishlist
+            return properties;
+
+        } catch (SQLException ex) {  // we are forced to catch SQLException
+            // don't let the SQLException leak from our DAO encapsulation
+            throw new DAOException(ex.getMessage(), ex);
+        }
+    }
+    
+    
 
     // method to sign users in
     // accesses getRenter() above
