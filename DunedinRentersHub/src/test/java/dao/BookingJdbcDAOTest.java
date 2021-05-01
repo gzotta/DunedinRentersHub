e95f2ef906d0c96913bc228df3f473f217cc4e4a
sarahaverill <sarahaverill@192.168.1.91>
2021-05-01 13:17:23 +1200

    Fix Booking test setup by creating Renter, Property and Landlord objects
    References #3

--- DunedinRentersHub/src/test/java/dao/BookingJdbcDAOTest.java
+++ DunedinRentersHub/src/test/java/dao/BookingJdbcDAOTest.java
@@ -6,9 +6,11 @@
 package dao;
 
 import domain.Booking;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import domain.Landlord;
+import domain.Property;
+import domain.Renter;
+import java.math.BigDecimal;
+import java.util.Date;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,7 +21,7 @@ import org.junit.jupiter.api.Test;
  */
 public class BookingJdbcDAOTest {
     
-    BookingJdbcDAO booking = new BookingJdbcDAO("jdbc:h2:mem:tests;INIT=runscript from 'src/main/java/dao/databaseSchema.sql'");
+   BookingJdbcDAO booking = new BookingJdbcDAO("jdbc:h2:mem:tests;INIT=runscript from 'src/main/java/dao/databaseSchema.sql'");
    
    private Booking booking1;
    private Booking booking2;
@@ -34,18 +36,30 @@ public class BookingJdbcDAOTest {
       booking2 = new Booking();
       booking3 = new Booking();
       
-     // booking1.setLandlordId(5678);
-     // booking1.setPropertyId(5658);
-     // booking1.setRenterId(7654);
+    Landlord land1 = new Landlord(1234, "lpass1", "luse1", "lphone1", "lemail1");
+    Property prop1 = new Property(5678, 4321, 3, "add1", "status1");
+    Renter rent1 =new Renter(5647, "rpass1", "ruse1", new Date(4/5/2000), "rphone1", "remail1", "rref1");
+      
+     booking1.setLandlord(land1);
+     booking1.setProperty(prop1);
+     booking1.setRenter(rent1);
      
-     // booking2.setLandlordId(5673);
-     // booking2.setPropertyId(5672);
-     // booking2.setRenterId(7054);
-      
-     // booking3.setLandlordId(6678);
-     // booking3.setPropertyId(5378);
-    //  booking3.setRenterId(7684);
-      
+    Landlord land2 = new Landlord(1224, "lpass2", "luse2", "lphone2", "lemail2");
+    Property prop2 = new Property(5628, 4221, 2, "add2", "status2");
+    Renter rent2 =new Renter(5627, "rpass2", "ruse2", new Date(4/2/2000), "rphone2", "remail2", "rref2");
+     
+     booking2.setLandlord(land2);
+     booking2.setProperty(prop2);
+     booking2.setRenter(rent2);
+     
+    Landlord land3 = new Landlord(1334, "lpass3", "luse3", "lphone3", "lemail3");
+    Property prop3 = new Property(5378, 4323, 1, "add3", "status3");
+    Renter rent3 =new Renter(5347, "rpass3", "ruse3", new Date(4/3/2000), "rphone3", "remail3", "rref3");
+    
+     booking3.setLandlord(land3);
+     booking3.setProperty(prop3);
+     booking3.setRenter(rent3);
+     
      booking.save(booking1);
      booking.save(booking2);    
          
