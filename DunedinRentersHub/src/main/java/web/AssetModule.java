6a285d82742cf58027816a3f7faee1bee209f5d2
gzotta <83261765+gzotta@users.noreply.github.com>
2021-05-12 23:41:45 +1200

    Assets added to assets module. #16

--- DunedinRentersHub/src/main/java/web/AssetModule.java
+++ DunedinRentersHub/src/main/java/web/AssetModule.java
@@ -10,11 +10,17 @@ import org.jooby.Results;
 public class AssetModule extends Jooby {
 
     public AssetModule() {
-        assets("/.html");
-        assets("/css/.css");
-        assets("/js/.js");
-        assets("/images/.png");
-        assets("/images/.jpg");
+        assets("/*.html");
+        assets("/css/*.css");
+        assets("/js/*.js");
+        assets("/images/properties/photos/*.png");
+        assets("/images/properties/photos/*.jpeg");
+        assets("/images/properties/maps/*.png");
+        assets("/images/properties/maps/*.jpeg");
+        assets("/images/renters/*.png");
+        assets("/images/renters/*.jpg");
+        assets("/images/services/*.png");
+        assets("/images/services/*.jpg");
 // make index.html the default page
         assets("/", "index.html");
 // prevent 404 errors due to browsers requesting favicons
